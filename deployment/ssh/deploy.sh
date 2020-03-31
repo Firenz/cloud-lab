e324ea7812cb2bcaeef9bbef2b49ee929db7551e
Alicia G <alicia.guardenoalbertos@gmail.com>
2020-03-31 19:45:26 +0200

    minor refactor

--- deployment/ssh/deploy.sh
+++ deployment/ssh/deploy.sh
@@ -5,8 +5,7 @@
 
 HOST="ec2-13-58-73-62.us-east-2.compute.amazonaws.com"
 USER="ec2-user"
-KEY="../../terraform/lemoncode.pem
-"
+KEY="../../terraform/lemoncode.pem"
 
 ssh -i $KEY $USER@$HOST \
   "sudo yum install docker -y && sudo service docker start && sudo docker run -d -p 80:8888 firenz/lemoncode-docker-exercise"
