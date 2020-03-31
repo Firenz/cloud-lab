cbe7cf0765ffd42e054cf705209c28c26497be43
Alicia G <alicia.guardenoalbertos@gmail.com>
2020-03-31 19:12:47 +0200

    move deployment folder outside of terraform folder

--- deployment/ssh/deploy.sh
+++ deployment/ssh/deploy.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+# This script is just an example of deployment without depending on terraform
+# The data provided worked in some instance, but maybe doesn't work anymore. 
+# Be careful using them as they are.
 
 HOST="ec2-13-58-73-62.us-east-2.compute.amazonaws.com"
 USER="ec2-user"
-KEY="../../lemoncode.pem"
+KEY="../../terraform/lemoncode.pem
+"
 
 ssh -i $KEY $USER@$HOST \
   "sudo yum install docker -y && sudo service docker start && sudo docker run -d -p 80:8888 firenz/lemoncode-docker-exercise"
